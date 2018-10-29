22deafa766041703b472ab5a3d72b736df945bfd
Josh Duff <me@JoshDuff.com>
2018-10-29 11:26:15 -0500

    Assert that the recombined calculation is only re-run once

--- test.js
+++ test.js
@@ -8,9 +8,16 @@ test(`some forking case`, t => {
 	const aDoubled = computed({ a }, ({ a }) => a * 2)
 	const bDoubled = computed({ b }, ({ b }) => b * 2)
 
-	const recombined = computed({ aDoubled, bDoubled }, ({ aDoubled, bDoubled }) => aDoubled + bDoubled)
+	let recombinedComputedCalculations = 0
 
+	const recombined = computed({ aDoubled, bDoubled }, ({ aDoubled, bDoubled }) => {
+		recombinedComputedCalculations += 1
+		return aDoubled + bDoubled
+	})
+
+	t.equal(recombinedComputedCalculations, 1)
 	t.equal(recombined.get(), 6)
 	a.set(3)
+	t.equal(recombinedComputedCalculations, 2)
 	t.equal(recombined.get(), 10)
 })
